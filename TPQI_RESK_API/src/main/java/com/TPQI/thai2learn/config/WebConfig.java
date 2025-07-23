f9a949b1712cfcbec2cb954ecb338b7b5b012cc4
tawannantawat <tawanswc@gmail.com>
2025-07-23 11:30:08 +0700

    chore: เพิ่มโปรไฟล์สำหรับสภาพแวดล้อม dev และ prod
    เพิ่มการตั้งค่าแยกสำหรับสภาพแวดล้อม development และ production
    เพื่อให้สามารถจัดการการตั้งค่าต่างๆ โดยเฉพาะเรื่องความปลอดภัยได้ง่ายขึ้น
    
    - สร้างไฟล์ `application-dev.properties` และ `application-prod.properties`
    - ปรับ `SecurityConfig` เพื่อปิดการตรวจสอบสิทธิ์ในโหมด `dev` ทำให้เรียก API เพื่อทดสอบได้สะดวก
    - โหมด `prod` ถูกตั้งค่าให้พร้อมเปิดใช้งานความปลอดภัยเต็มรูปแบบ
    - กำหนดให้ `dev` เป็นโปรไฟล์เริ่มต้น (default)

--- TPQI_RESK_API/src/main/java/com/TPQI/thai2learn/config/WebConfig.java
+++ TPQI_RESK_API/src/main/java/com/TPQI/thai2learn/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://119.59.100.55:4175")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); 
